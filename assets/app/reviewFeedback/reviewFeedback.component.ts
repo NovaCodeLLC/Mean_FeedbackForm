9a16d5787c8df3683a1ef120406293c3ed395879
NovaCodeLLC <Tjlesperance@NovacodeLLC.com>
2017-03-18 12:33:40 -0400

    Changed the reviewFeedback HTML to have an onBlur event: event now passes contentText of the corresponding upsBox / downsBox to the onChangeMethod in the component class Added onChangeMethod to reviewFeedback.component.ts.  This method updates the feedback model with the user's changes made in the DOM.

--- assets/app/reviewFeedback/reviewFeedback.component.ts
+++ assets/app/reviewFeedback/reviewFeedback.component.ts
@@ -2,7 +2,7 @@
  * Created by TXL8009 on 3/16/2017.
  */
 
-import {Component, OnInit, Input} from "@angular/core";
+import {Component, Input} from "@angular/core";
 
 import {reviewFeedbackService} from "./reviewFeedback.service";
 import {Feedback} from "../feedbackSubmission/feedback.model";
@@ -21,6 +21,23 @@ export class reviewFeedbackComponent{
     @Input() feedback : Feedback
 
     onEditMethod(){
+        console.log(this.feedback);
+    }
+
+    changeData(newValue : string, propertyName : String){
+        switch (propertyName){
+
+            case "upsBox":
+                if(newValue != null){
+                this.feedback.upsBox = newValue;
+                }
+
+            case "downsBox":
+                if(newValue != null){
+                    this.feedback.downsBox = newValue;
+                }
+        }
+
         console.log(this.feedback);
     }
 
